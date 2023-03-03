df8fed199b66db1e616e1a04508f54d32e66765c
daniela marques <dd978452@gmail.com>
2023-03-03 16:43:23 -0300

    incluindo javascript no site

---
+++ javascript.js
@@ -0,0 +1,49 @@
+ setTimeout(ApresentarModal, 5000);
+ function ApresentarModal(){
+    var modal= document.querySelector(".modal");
+
+    if(modal != null){
+        document.querySelector(".modal").style.display = "block";
+
+        document.querySelector(".modal a")
+        .addEventListener("click", function(){
+
+
+        document.querySelector(".modal").style.display = "none";
+
+ });
+}
+}
+
+if(document.forms["modal_form"] != undefined){
+
+    var form = document.forms["modal_form"];
+
+    form.addEventListener("submit", validarFormModal);
+
+}
+
+function validarFormModal(evt){
+    var form = document.forms["modal_form"];
+
+    var inputEmail = form.email;
+    var valorEmail = form.email.value;
+
+    var posicaoArroba = 
+    valorEmail.indexOf("@");
+
+    if( 
+        valorEmail != "" &&
+        valorEmail.indexOf("@") > 3 && 
+        valorEmail.lastIndexOf(".") > posicaoArroba
+    ){
+
+       
+    }
+    else{
+        
+        inputemail.classname="nao_valido";
+        document.querySelector("span.nao_valido").style.display = "block";
+        evt.preventDefault();
+    }
+}
